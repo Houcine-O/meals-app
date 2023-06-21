efd137e60c2ae7397862559617fa851584022431
Houcine <salimangry3150@gmail.com>
2023-06-21 17:59:07 +0100

    added a side drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/main.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/widgets/custom_drawer.dart';
 
 import '../models/meal.dart';
 
@@ -17,9 +19,27 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _toggleFavMeals(Meal meal) {
     if (_favoriteMeals.contains(meal))
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "Recipe removed from your favorites",
+            style: TextStyle(color: Theme.of(context).colorScheme.background),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.onBackground,
+        ));
+      });
     else
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "Recipe added to your favorites",
+            style: TextStyle(color: Theme.of(context).colorScheme.background),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.onBackground,
+        ));
+      });
   }
 
   void _selectItem(int index) {
@@ -44,6 +64,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: _selectedItemIndex == 1 ? Text('Favorites') : Text('Categories'),
       ),
+      drawer: const CustomDrawer(),
       body: activeScreen,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedItemIndex,
